906013ef7ba689d3a7e5b285bc9c4a341193b0b3
amallasheen <amalwalied@gmail.com>
2015-11-15 00:16:12 +0200

    added edit and update and show in users controller,managed routes

--- Bachelor/app/helpers/users_helper.rb
+++ Bachelor/app/helpers/users_helper.rb
@@ -1,2 +1,23 @@
 module UsersHelper
+	 def log_in(user)
+        session[:user_id] = user.id
+        self.current_user = user
+    end
+    #setter method
+    def current_user=(user)
+        @current_user = (user)
+    end
+    #getter method
+    def current_user
+        @current_user = User.find(1) 
+    end
+    def logged_in?
+        !current_user.nil?
+    end
+
+    def sign_out
+        session[:user_id] = nil
+        redirect_to root_path
+    end
+
 end
--- Bachelor/app/models/follow.rb
+++ Bachelor/app/models/follow.rb
@@ -1,4 +1,18 @@
 class Follow < ActiveRecord::Base
-  belongs_to :user, foreign_key: "follower_id"
-  belongs_to :user, foreign_key: "followee_id"
+  #belongs_to :user, foreign_key: "follower_id"
+  #belongs_to :user, foreign_key: "followee_id"
+  belongs_to :follower, class_name: 'User'
+  belongs_to :followee, class_name: 'User'
+  validates_presence_of(:follower_id)
+  validates_presence_of(:followee_id)
+  #validates_associated :follower, :followee
+  validates_uniqueness_of :follower_id, :scope => [:followee_id]
+  validate :different_user
+
+
+  def different_user
+  	if follower_id == followee_id
+  		errors.add(:follower_id, "can't follow themselves")
+  	end
+  end
 end
