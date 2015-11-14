4cce27ceb4bf5dba8a3f4413669b4acc100a4052
sohaghareb <soha.ghareb@student.guc.edu.eg>
2015-11-15 01:57:31 +0200

    create comments controller and have tested it

--- Bachelor/app/controllers/comments_controller.rb
+++ Bachelor/app/controllers/comments_controller.rb
@@ -1,19 +1,40 @@
 class CommentsController < ApplicationController
+
+def index
+        @comments=Comment.where(:post_id=>params(:id))
+  end
+
+
+  def destroy
+     @comment=Comment.find(params[:id])
+     @comment.destroy
+     render :back
+  end
+
+
   def new
+    @commnet = Comment.new
   end
 
-  def create
+def create
+
+    @comment = Comment.new(comment_params)
+    @comment.post_id=params[:post_id]
+
+    if @comment.save
+      #redirect_to :back
+      redirect_to posts_path
+     #render Rails.application.routes.recognize_path(request.referer)[:action]
+    else
+      render :back
+    end
   end
 
-  def edit
-  end
 
-  def delete
-  end
 
-  def index
-  end
-
-  def show
-  end
+  private
+    def comment_params
+      params.require(:comment).permit(:content,:post_id)#have to add user_id after the session
+    end
 end
+
--- Bachelor/config/routes.rb
+++ Bachelor/config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  get 'comments/new'
-
-  get 'comments/create'
-
-  get 'comments/edit'
-
-  get 'comments/delete'
-
-  get 'comments/index'
-
-  get 'comments/show'
+ 
 
   resources :posts
+  resources :comments
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
