3382a3080206da8d8a1747f19652132e7d1577f3
amallasheen <amalwalied@gmail.com>
2015-11-15 18:59:18 +0200

    Added GUC email and dob validations

--- Milestone1/app/models/user.rb
+++ Milestone1/app/models/user.rb
@@ -6,6 +6,12 @@ class User < ActiveRecord::Base
   has_many :followees, class_name: 'Follow', :foreign_key => 'followee_id'
   validates :fname, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
   validates :lname, format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
-  validates :email, presence: true, uniqueness: true
   validates_inclusion_of :doctor, :in => [true, false]
+  validates :email, presence: true, uniqueness: true, format: {with: /\A\w+\-?\w*\.\w*\-?\w*@\w*\.?guc.edu.eg\z/,message: "insert GUC email"}
+  validate :dob_validation
+  def dob_validation 
+     if !dob.nil? && dob > Date.today
+     	errors.add(:dob, "can't be in the future")
+     end  
+   end 
 end
