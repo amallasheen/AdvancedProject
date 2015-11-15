b9d79683826ad311791bb71630b58912b2aed3b0
Mostafa Gohar <mostafa-gohar@hotmail.com>
2015-11-15 09:37:32 +0200

    changed has_and_belongs_to_many to has_many in post.rb

--- Milestone1/app/models/post.rb
+++ Milestone1/app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_and_belongs_to_many :comments
+  has_many :comments
   validates_presence_of(:content)
   validates_presence_of(:user_id)
 end
