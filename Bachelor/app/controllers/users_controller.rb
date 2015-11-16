52eb7a3a71db7815ff1de5006a84920c207acbca
weammahmoud <engineer_weammahmoud@yahoo.com>
2015-11-16 14:55:44 +0200

    timeline done for followed users .

--- Bachelor/app/controllers/users_controller.rb
+++ Bachelor/app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-		user_params = User.new(params.require(:user).permit(:email, :gucid, :fname , :lname , :dob, :gender, :location, :avatar))
+		user_params = params.require(:user).permit(:email, :gucid, :fname , :lname , :dob, :gender, :location, :avatar)
 		user_params
   end
  	
