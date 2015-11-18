baa14fe117c34ec1894c0cc51a2626e923b5a24c
MostafaGohar <mostafa-gohar@hotmail.com>
2015-11-18 23:07:54 +0200

    changed type to doctor in Bachelor folder

--- Bachelor/db/migrate/20151110092827_create_users.rb
+++ Bachelor/db/migrate/20151110092827_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.boolean :type
+      t.boolean :doctor
       t.string :email
       t.string :gucid
       t.string :fname
